4a5a1293ed4385300d12ce3ea25eed4e03038638
Pungo Token <45178410+pungotoken@users.noreply.github.com>
2019-05-31 02:02:16 +0200

    Update networks.js

--- src/networks.js
+++ src/networks.js
@@ -11,25 +11,92 @@ exports.bitcoin = {
   scriptHash: 0x05,
   wif: 0x80,
 };
-exports.regtest = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bcrt',
-  bip32: {
-    public: 0x043587cf,
-    private: 0x04358394,
+
+module.exports = {
+  bitcoin: {
+    magic: 0xd9b4bef9,
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x00,
+    scriptHash: 0x05,
+    wif: 0x80,
+    dustThreshold: 546, // https://github.com/bitcoin/bitcoin/blob/v0.9.2/src/core.h#L151-L162
+    isPoS: false
   },
-  pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
-  wif: 0xef,
-};
-exports.testnet = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'tb',
-  bip32: {
-    public: 0x043587cf,
-    private: 0x04358394,
+  testnet: {
+    magic: 0xd9b4bef9,
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bip32: {
+      public: 0x043587cf,
+      private: 0x04358394
+    },
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
+    wif: 0xef,
+    dustThreshold: 546,
+    isPoS: false
   },
-  pubKeyHash: 0x6f,
-  scriptHash: 0xc4,
-  wif: 0xef,
-};
+  litecoin: {
+    magic: 0xd9b4bef9,
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bip32: {
+      public: 0x019da462,
+      private: 0x019d9cfe
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x05,
+    wif: 0xb0,
+    dustThreshold: 0, // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.cpp#L360-L365
+    isPoS: false
+  },
+  dogecoin: {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e,
+    dustThreshold: 0, // https://github.com/dogecoin/dogecoin/blob/v1.7.1/src/core.h#L155-L160
+    isPoS: false
+  },
+  
+  zelcash: {
+    messagePrefix: '\x18ZelCash Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1cb8,
+    scriptHash: 0x1cbd,
+    wif: 0x80,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    dustThreshold: 0,
+    isPoS: false
+  },
+  
+  peercoin: {
+    messagePrefix: '\x17PPcoin Signed Message:\n',
+    bip32: {
+      public: 0x01da950b,  // start with "Ppub..."
+      private: 0x01da90d0  // start with "Pprv..."
+    },
+    pubKeyHash: 0x37,
+    scriptHash: 0x75,
+    wif: 0xb7,
+    dustThreshold: 0,
+    isPoS: true
+  },
+}
